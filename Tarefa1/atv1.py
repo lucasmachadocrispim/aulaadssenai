0ba03a4ae7539a41d452e9d987bc96e86b84afa2
Lucas Machado Crispim <lucasmachadocrispim@gmail.com>
2026-04-15 22:18:04 +0000

    Final Atv 1

--- Tarefa1/atv1.py
+++ Tarefa1/atv1.py
@@ -1,5 +1,5 @@
 class Livro:
-    def __init__(self, titulo, autor, disponivel):
+    def __init__(self, titulo, autor):
         self.__titulo = titulo
         self.__autor = autor
         self.__disponivel = True
@@ -31,10 +31,55 @@ class Biblioteca:
         titulo = input("Insira o título do livro que deseja emprestar: ")
         livro = self.encontrar_livro(titulo)
         if livro:
-            
+            if livro.get_disponivel():
+                livro.emprestar()
+                print("Livro emprestado com sucesso.")
+            else:
+                print("O livro não está disponível para empréstimo.")
         else:
-            print("Livro não encontrado")
+            print("Livro não encontrado.")
     def devolver_livro(self):
-        return self.a * self.b
+        titulo = input("Insira o título do livro que deseja devolver: ")
+        livro = self.encontrar_livro(titulo)
+        if livro:
+            if not livro.get_disponivel():
+                livro.devolver()
+                print("Livro devolvido com sucesso.")
+            else:
+                print("O livro não está disponível para devolução.")
+        else:
+            print("Livro não encontrado.")
     def listar_livros(self):
-        return self.a / self.b
+        if not self.livros:
+            print("Nenhum livro está cadastrado")
+            return
+        print("Lista de livros:")
+        for livro in self.livros:
+            status = "Disponível" if livro.get_disponivel() else "Indisponivel"
+            print("Nome do livro:", livro.get_titulo(), " -  Nome do autor:", livro.get_autor(), " -  Status:", status)
+def main():
+    biblioteca = Biblioteca()
+    while True:
+        print("\n" + "="*50)
+        print("---Menu da Biblioteca---\n")
+        print("1 - Cadastrar um Livro")
+        print("2 - Emprestar um Livro")
+        print("3 - Devolver um Livro")
+        print("4 - Listar Livros")
+        print("5 - Sair do Sistema\n")
+        escolha = input("Digite a opção que você deseja: ")
+        print()
+        if escolha == "1":
+            biblioteca.cadastrar_livro()
+        elif escolha == "2":
+            biblioteca.emprestar_livro()
+        elif escolha == "3":
+            biblioteca.devolver_livro()
+        elif escolha == "4":
+            biblioteca.listar_livros()
+        elif escolha == "5":
+            print("Encerrando o sistema")
+            break
+        else:
+            print("Opção inválida, tente novamente.")
+main()
