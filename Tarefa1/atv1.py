5c5a5c648c76f5d7798f3f11cc308a260f79ab9a
Lucas Machado Crispim <lucasmachadocrispim@gmail.com>
2026-04-15 21:36:58 +0000

    Mudancas

--- Tarefa1/atv1.py
+++ Tarefa1/atv1.py
@@ -15,15 +15,25 @@ class Livro:
         self.__disponivel = True
 class Biblioteca:
     def __init__(self):
-        self.__livros = []
-
+        self.livros = []
     def cadastrar_livro(self):
         titulo = input("Digite o título do livro: ")
         autor = input("Digite o autor do livro: ")
         livro = Livro(titulo, autor)
-        return self.a + self.b
+        self.livros.append(livro)
+        print("Livro cadastrado com sucesso")
+    def encontrar_livro(self, titulo):
+        for livro in self.livros:
+            if livro.get_titulo() == titulo:
+                return livro
+            return None
     def emprestar_livro(self):
-        return self.a - self.b
+        titulo = input("Insira o título do livro que deseja emprestar: ")
+        livro = self.encontrar_livro(titulo)
+        if livro:
+            
+        else:
+            print("Livro não encontrado")
     def devolver_livro(self):
         return self.a * self.b
     def listar_livros(self):
